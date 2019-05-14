b5a8dd886d23ff385505e4ad97e73ee7ff1db3a8
“Softwarefeeder” <softwarefeeder@gmail.com>
2019-05-14 08:47:44 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,50 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {Platform, StyleSheet, TouchableOpacity, Image} from 'react-native';
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  android:
+    'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+type Props = {};
+export default class App extends Component<Props> {
+
+ onPressButton() {
+   console.log("touch log message");
+}
+
+  render() {
+    return (
+      <TouchableOpacity onPress={this.onPressButton}>
+      <Image
+        style={styles.styleForTouch}
+        source={require('./demoimage.png')}
+      />
+    </TouchableOpacity>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+ 
+  styleForTouch: {
+    justifyContent: 'center',
+    alignItems: 'stretch'
+    
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
